f58a7669c7ed15924b451f5c123c1cf814bb898f
DkStine <dkctcak@gmail.com>
2023-07-16 20:37:01 +0530

    commit

--- 2/02_03.cpp
+++ 2/02_03.cpp
@@ -3,18 +3,66 @@ using namespace std;
 
 int main()
 {
-    int money;
+    // int money;
 
-    cout<<"Enter the money you have: ";
-    cin>>money;
+    // cout<<"Enter the money you have: ";
+    // cin>>money;
 
-    if (money > 5000)
+    // if (money > 5000)
+    // {
+    //     cout<<"Go with Neha\n";
+    // }
+    // else
+    // {
+    //     cout<<"Go with Sneha\n";
+    // }
+
+    // Practice_Q1:
+    // int num1, num2, num3;
+
+    // cout << "Enter number 1: ";
+    // cin >> num1;
+    // cout << "Enter number 2: ";
+    // cin >> num2;
+    // cout << "Enter number 3: ";
+    // cin >> num3;
+
+    // if (num1 > num2)
+    // {
+    //     if (num1 > num3)
+    //     {
+    //         cout << "The greatest is: " << num1;
+    //     }
+    //     else
+    //     {
+    //         cout << "The greatest is: " << num3;
+    //     }
+    // }
+    // else
+    // {
+    //     if (num2 > num3)
+    //     {
+    //         cout << "The greatest is: " << num2;
+    //     }
+    //     else
+    //     {
+    //         cout << "The greatest is: " << num3;
+    //     }
+    // }
+
+    // Practice_Q2
+    int n;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    if (n % 2 == 0)
     {
-        cout<<"Go with Neha\n";
+        cout<<"It's even.";
     }
     else
     {
-        cout<<"Go with Sneha\n";
+        cout<<"It's odd.";
     }
 
     return 0;
