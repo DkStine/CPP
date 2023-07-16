5bde70b0b53943dd1533864dde3830ffa0c128c3
DkStine <dkctcak@gmail.com>
2023-07-16 21:00:39 +0530

    commit

---
+++ 2/02_04.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n, sum = 0;
+
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+
+    // cout<<"The sum of all integers upto "<<n<<" is "<<sum;
+
+    // int n = 0;
+
+    // do
+    // {
+    //     cout<<"Enter a number: ";
+    //     cin>>n;
+    // }
+    // while (n >= 0);
+
+    // Practice_Q1
+    int n;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+    }
+
+    return 0;
+}
