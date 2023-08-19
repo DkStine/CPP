455453f2fde4df6a1bc95ade527ec611e609fef1
DkStine <dkctcak@gmail.com>
2023-08-19 22:20:26 +0530

    commit

--- C++ course/04_01.cpp
+++ C++ course/04_01.cpp
@@ -7,7 +7,7 @@ int main()
     // {
     //     for (int j = 1; j <= 4; j++)
     //     {
-    //         cout << "* "; 
+    //         cout << "* ";
     //     }
     //     cout << endl;
     // }                                    --> Filled Rectangle
@@ -37,15 +37,46 @@ int main()
     //     cout << "* ";
     // }                                                --> Hollow rectangle
 
+    // int n;
+    // cout << "Enter: ";
+    // cin >> n;
+
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n;
+    // cout << "Enter: ";
+    // cin >> n;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n - i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = 1; k <= i; k++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+        
+    // }
+
     int n;
     cout << "Enter: ";
     cin >> n;
 
-    for (int i = n; i >= 1; i--)
+    for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << "* ";
+            cout << i << " ";
         }
         cout << endl;
     }
